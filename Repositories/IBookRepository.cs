65a59a92a50f9edd134e4867530bfe7c0e21957b
cattfan <cattfan239@gmail.com>
2025-09-21 15:52:25 +0700

    P4

---
+++ Repositories/IBookRepository.cs
@@ -0,0 +1,15 @@
+﻿using BaithuchanhWeb2.Models.Domain;
+using BaithuchanhWeb2.Models.DTO;
+
+namespace BaithuchanhWeb2.Repositories
+{
+    public interface IBookRepository
+    {
+        List<BookWithAuthorAndPublisherDTO> GetAllBooks();
+        BookWithAuthorAndPublisherDTO GetBookById(int id);
+        AddBookRequestDTO AddBook(AddBookRequestDTO addBookRequestDTO);
+        AddBookRequestDTO? UpdateBookById(int id, AddBookRequestDTO bookDTO);
+        Book? DeleteBookById(int id);
+    }
+
+}
